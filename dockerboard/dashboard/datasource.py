13f9d313f4456755d4431b10c8f86aad4c34cb5a
Sebastian Łach <root@slach.eu>
2019-10-28 11:53:28 +0100

    Add dashboard view

---
+++ dockerboard/dashboard/datasource.py
@@ -0,0 +1,24 @@
+from io import StringIO
+from pandas import read_csv, DataFrame
+from requests import get
+
+
+class AdverityReader(object):
+
+    data = None
+    columns = ('Date', 'Datasource', 'Campaign', 'Clicks', 'Impressions')
+
+    def __init__(self, url):
+        """Construct reader with given URL."""
+        self.url = url
+
+    def get_data_frame(self):
+        """Get data frame from given URL."""
+        if not self.data:
+            try:
+                content = get(self.url).text
+                self.data = read_csv(StringIO(content))
+            except ValueError as e:
+                return DataFrame(columns=self.columns)
+
+        return self.data
---
+++ dockerboard/dashboard/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = dict()
+    return render(request, 'dashboard/index.html', context)
+
