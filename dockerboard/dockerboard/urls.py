a702b59eb849b4e6c19f30a513630c343589905f
Sebastian Łach <root@slach.eu>
2019-10-30 14:21:57 +0100

    Add static files

--- dockerboard/dockerboard/urls.py
+++ dockerboard/dockerboard/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^$', include('dashboard.urls', namespace='dashboard')),
     url(r'^containers/', include('containers.urls', namespace='containers')),
     url(r'^images/', include('images.urls', namespace='images')),
-]
+] + static(settings.STATIC_URL)
